30754cb64b50c66a6dcbda69cb5b0d73de7a9278
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-09-19 20:43:45 +0300

    Create Agent_parser.py

---
+++ Agent/Agent_parser.py
@@ -0,0 +1,20 @@
+from langchain.agents import AgentOutputParser
+from langchain.schema import AgentAction, AgentFinish
+from typing import Union
+import re
+
+class CustomOutputParser(AgentOutputParser):
+    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+        if "Final Answer:" in llm_output:
+            return AgentFinish(
+                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
+                log=llm_output,
+            )
+        
+        regex = r"Action: (.*?)[\n]*Action Input:[\s]*(.*)"
+        match = re.search(regex, llm_output, re.DOTALL)
+        if not match:
+            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+        action = match.group(1).strip()
+        action_input = match.group(2)
+        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
