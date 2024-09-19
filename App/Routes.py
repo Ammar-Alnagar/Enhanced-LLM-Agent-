4731dc0b8fd363d2f48eee49b9aec1e1a499ec6a
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-09-19 20:41:33 +0300

    Create Routes.py

---
+++ App/Routes.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, request, jsonify
+from agent.agent import create_agent_executor
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/query', methods=['POST'])
+def query():
+    data = request.json
+    question = data['question']
+    agent_executor = create_agent_executor()
+    result = agent_executor.run(question)
+    return jsonify({'result': result})
